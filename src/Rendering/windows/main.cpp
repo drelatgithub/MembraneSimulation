7fed881da8c3675841adc047888fd75e2167db17
drelatgithub <drel@sohu.com>
2017-10-07 17:36:55 -0400

    Added lines according to tutorial.

--- src/Rendering/windows/main.cpp
+++ src/Rendering/windows/main.cpp
@@ -1,6 +1,15 @@
 #include"glew/glew.h"
 #include"GLFW/glfw3.h"
 
+#include"common.h"
+
 int main() {
+
+	// Initialize GLFW
+	if (!glfwInit()) {
+		LOG(ERROR) << "Failed to initialize GLFW";
+		return -1;
+	}
+
 	return 0;
 }
