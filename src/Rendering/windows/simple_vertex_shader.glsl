31a2f667cedeb61ebc327416d2c8a128ee6dcff2
Drel <drel@sohu.com>
2017-10-11 18:21:35 -0400

    Include glm in the program

--- src/Rendering/windows/simple_vertex_shader.glsl
+++ src/Rendering/windows/simple_vertex_shader.glsl
@@ -1,7 +1,11 @@
 #version 330 core
+
+// Input vertex data
 layout(location = 0) in vec3 vertex_pos_model;
 
+// The model-view-projection matrix
+uniform mat4 mvp;
+
 void main(){
-    gl_Position.xyz = vertex_pos_model;
-    gl_Position.w = 1.0;
+    gl_Position = mvp * vec4(vertex_pos_model, 1);
 }
