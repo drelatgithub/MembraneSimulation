62e1f8368366bfcded4c1673315a811dc3868084
drelatgithub <drel.sohu.com@gmail.com>
2017-12-15 18:39:40 -0500

    Change in the converter

--- output_manip/mesh-to-medyan.py
+++ output_manip/mesh-to-medyan.py
@@ -12,9 +12,10 @@ file_out_dir = cur_dir + "\\"
 file_out = file_out_dir + "sphere.txt"
 
 len_ref = 1e9
+coo_mov = [1500, 1500, 1500]
 
 with open(file_in[0], 'r') as pos, open(file_in[1], 'r') as nei, open(file_out, 'w') as out:
     for pos_line, nei_line in zip(pos, nei):
-        for each_num in pos_line.strip().split('\t'):
-            out.write(str(float(each_num) * len_ref) + '\t')
+        for each_num, each_mov in zip(pos_line.strip().split('\t'), coo_mov):
+            out.write(str(float(each_num) * len_ref + each_mov) + '\t')
         out.write(nei_line)
